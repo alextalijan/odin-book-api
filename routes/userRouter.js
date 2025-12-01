a273b555aeecd3668f9de01d33ec4dd684608c61
alextalijan <acatalijan@icloud.com>
2025-12-01 16:20:15 -0600

    feat: add route for logging users in

--- routes/userRouter.js
+++ routes/userRouter.js
@@ -6,5 +6,6 @@ const controller = require('../controllers/userController');
 
 router.post('/', controller.register);
 router.get('/me', controller.returnUser);
+router.get('/login', controller.login);
 
 module.exports = router;
