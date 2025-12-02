430365d8744c01edc900c1666ccb147f957f3fca
alextalijan <acatalijan@icloud.com>
2025-12-01 19:58:59 -0600

    feat: write middleware for authenticating every request

---
+++ utils/authenticateToken.js
@@ -0,0 +1,23 @@
+const jwt = require('jsonwebtoken');
+
+function authenticateToken(req, res, next) {
+  const token = req.cookies.jwt;
+
+  if (!token) {
+    return res
+      .sendStatus(401)
+      .json({ success: false, message: 'No token provided.' }); // No token provided
+  }
+
+  jwt.verify(token, process.env.JWT_SECRET, (err, decoded) => {
+    if (err) {
+      return res
+        .sendStatus(403)
+        .json({ success: false, message: 'Invalid or expired token.' }); // Invalid or expired token
+    }
+    req.user = decoded.sub; // Attach decoded payload to request
+    next(); // Proceed to the next middleware or route handler
+  });
+}
+
+export default authenticateToken;
