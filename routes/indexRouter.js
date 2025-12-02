f129a072975b9a43cf748625a329060e17dd39ea
alextalijan <acatalijan@icloud.com>
2025-12-01 18:49:31 -0600

    fix: remove typo utilizing userController in indexRouter

--- routes/indexRouter.js
+++ routes/indexRouter.js
@@ -2,7 +2,7 @@ const { Router } = require('express');
 const router = Router();
 
 // Import controller
-const controller = require('../controllers/userController');
+const controller = require('../controllers/indexController');
 
 router.post('/login', controller.login);
 router.post('/logout', controller.logout);
