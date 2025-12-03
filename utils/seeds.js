8c713825dbf97fb0b0db4bd6e73d6de84530e3ed
alextalijan <acatalijan@icloud.com>
2025-12-02 19:21:05 -0600

    feat: write faker script that adds 5 fake people and their posts

---
+++ utils/seeds.js
@@ -0,0 +1,57 @@
+const { PrismaClient } = require('@prisma/client');
+const prisma = new PrismaClient();
+const { faker } = require('@faker-js/faker');
+
+// Function that creates fake users
+function createRandomUser() {
+  return {
+    id: faker.string.uuid(),
+    username: faker.internet.username(),
+    hash: faker.internet.password(),
+  };
+}
+
+async function main() {
+  // Find the id of alex
+  const alex = await prisma.user.findUnique({
+    where: {
+      username: 'alex',
+    },
+    select: {
+      id: true,
+    },
+  });
+
+  // Create 5 fake users
+  const users = faker.helpers.multiple(createRandomUser, {
+    count: 5,
+  });
+
+  // For each user
+  for (const user of users) {
+    // Add user to the database
+    await prisma.user.create({
+      data: {
+        id: user.id,
+        username: user.username,
+        hash: user.hash,
+      },
+    });
+
+    // Create fake post
+    await prisma.post.create({
+      data: {
+        text: `This is the first post made by ${user.username}.`,
+        authorId: user.id,
+        postedAt: faker.date.past(),
+      },
+    });
+
+    // Set alex to follow them
+    await prisma.following.create({
+      data: { followedId: user.id, followerId: alex.id },
+    });
+  }
+}
+
+main();
