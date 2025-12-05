b3b1ccabfb72a77c19698f6c51e0fbd79ef6675f
alextalijan <acatalijan@icloud.com>
2025-12-05 15:13:53 -0600

    feat: add route for returning the post info

--- controllers/postController.js
+++ controllers/postController.js
@@ -2,6 +2,65 @@ const { PrismaClient } = require('@prisma/client');
 const prisma = new PrismaClient();
 
 module.exports = {
+  getPost: async (req, res) => {
+    // Fetch the post and check if it exists
+    const post = await prisma.post.findUnique({
+      where: {
+        id: req.params.postId,
+      },
+      select: {
+        id: true,
+        text: true,
+        author: {
+          select: {
+            id: true,
+            username: true,
+          },
+        },
+        comments: {
+          select: {
+            text: true,
+            author: {
+              select: {
+                username: true,
+              },
+            },
+            commentedAt: true,
+          },
+          orderBy: {
+            commentedAt: 'desc',
+          },
+        },
+        _count: {
+          select: {
+            likes: true,
+            comments: true,
+          },
+        },
+        postedAt: true,
+      },
+    });
+    if (!post) {
+      return res.json({ success: false, message: 'Post not found.' });
+    }
+
+    // Check if the user follows the author of this post
+    const followings = await prisma.following.findMany({
+      where: {
+        followerId: req.user.id,
+        followedId: post.author.id,
+      },
+    });
+    if (followings.length === 0) {
+      return res.json({
+        success: false,
+        message: 'You do not follow this person.',
+      });
+    }
+
+    // Return the post details
+    res.json({ success: true, post });
+  },
   likePost: async (req, res) => {
     // Check if the post exists
     const post = await prisma.post.findUnique({
--- routes/postRouter.js
+++ routes/postRouter.js
@@ -5,6 +5,7 @@ const authenticateToken = require('../utils/authenticateToken');
 // Import controller
 const controller = require('../controllers/postController');
 
+router.get('/:postId', authenticateToken, controller.getPost);
 router.post('/:postId/likes', authenticateToken, controller.likePost);
 router.delete('/:postId/likes', authenticateToken, controller.unlikePost);
 
